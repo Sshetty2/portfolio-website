a8532bdde2a5119fb4f40f080b24efbcc1ba1a47
Sachit Shetty <sachit.shetty@gmail.com>
2024-12-17 10:37:52 -0500

    update projects

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -12,6 +12,14 @@ const projects = [
     tech       : ['React', 'Typescript', 'MUI', 'IBM Watson', 'AWS Lambda', 'API Gateway', 'Wikipedia API'],
     githubLink : 'https://https://github.com/Sshetty2/wiki-sentiment-analysis',
     projectLink: 'https://www.wiki-sentiment-analyzer.wiki/'
+  },
+  {
+    title      : 'Demo Blotter App',
+    // eslint-disable-next-line max-len
+    description: 'This React-based blotter app features an order entry form with validation for market and limit orders, real-time order tracking using AG Grid, and intuitive Buy/Sell color coding. Built with Redux for state management and Ant Design for a polished UI, the app supports responsive design, diverse order types, and advanced form controls, ensuring smooth user interaction and performance.',
+    tech       : ['React', 'Typescript', 'Redux', 'Ant Design', 'AG Grid', 'SCSS'],
+    githubLink : 'https://github.com/Sshetty2/demo-blotter-app',
+    projectLink: 'main.d1m98d5rusf0mr.amplifyapp.com/'
   }
 ];
 
@@ -28,7 +36,7 @@ const Project = ({ projectLink, githubLink, title, description, tech }: ProjectP
     <div className="project">
       <header>
         {/*  eslint-disable-next-line max-len */}
-        <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"> <title>Folder</title> <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path> </svg>
+        <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#57ffff" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"> <title>Folder</title> <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path> </svg>
         <div className="project-links">
           <a href={githubLink} target="_blank" rel="noreferrer">
             <img src={githubIcon} alt="Visit site" /></a>
