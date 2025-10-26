1febba26c19b95027e29296f26365d068906f6b6
Sachit Shetty <sachit.shetty@gmail.com>
2025-10-26 05:31:21 -0400

    Force new AppSync API key on deploy
    Overrides the logical ID of the AppSync API key resource to ensure a new key is generated on each deployment. This can help recover or rotate API keys as needed.

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,7 +2,12 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 
-defineBackend({
+const backend = defineBackend({
   auth,
-  data,
+  data
 });
+
+// 👇 Force a new AppSync API key resource for one deploy
+backend.data.resources.cfnResources.cfnApiKey?.overrideLogicalId(
+  `recoverApiKey${Date.now()}`
+);
